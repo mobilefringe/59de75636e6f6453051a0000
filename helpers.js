374df6dd025f899247aa1c9d83cbb7f6c4b57016
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-02-05 12:10:53 -0500

    naeim's twinpines 1  /helpers.js

--- helpers.js
+++ helpers.js
@@ -66,8 +66,8 @@ var love = setInterval(function() {
         }
     });
 }, 500);
-console.log()
-    console.log($(".bg_heart").css("height" ,$("body").height() +"px"));
+console.log($("body").height())
+    console.log($(".bg_heart").css("height" ,$("body").offsetHeight() +"px"));
     console.log($(".bg_heart").css("height"));
 }
 
